4fd2139b86631b0ec62617a944fa9467419ed76c
Taqmeel Zubeir <taqmeelzubeir.dev@gmail.com>
2020-07-08 19:40:37 +0530

    Add - appdata.xml and .desktop files

--- meson/post_install.py
+++ meson/post_install.py
@@ -3,8 +3,14 @@
 from os import path, environ
 from subprocess import call
 
-schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+schema_dir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+data_dir = path.join(environ.get('MESON_INSTALL_PREFIX', '/usr/local'), 'share')
+desktop_database_dir = path.join(data_dir, 'applications')
 
-if not environ.get ('DESTDIR'):
-    print('Compiling GSetting Schemas…')
-    call(['glib-compile-schemas', schemadir])
+if not environ.get('DESTDIR'):
+    print('Compiling GSettings Schemas…')
+    call(['glib-compile-schemas', schema_dir])
+    print('Updating Desktop Database…')
+    call(['update-desktop-database', '-q', desktop_database_dir])
+    print('Updating Icon Cache…')
+    call(['gtk-update-icon-cache', '-qtf', path.join(data_dir, 'icons', 'hicolor')])
